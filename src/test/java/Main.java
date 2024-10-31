6cacd1404239ae89d90d5bde59440fd1d87d2ba3
belaletech <belalahmad@lambdatest.com>
2024-10-31 18:04:24 +0530

    upload

---
+++ src/test/java/Main.java
@@ -0,0 +1,62 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.HashMap;
+
+public class Main {
+    private RemoteWebDriver driver;
+
+    @BeforeTest
+    public void setup() throws Exception {
+        // LambdaTest credentials
+        String username = "belalahmad";
+        String authkey = "sZhRHD9cmkV1nXNWEUw7psGAxRgN84ugdKqGMforKfXJCQhIU7";
+        String hub = "hub.lambdatest.com/wd/hub";
+
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 10");
+        browserOptions.setBrowserVersion("129");
+        HashMap<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("build", "windowsChrome");
+//        ltOptions.put("browserName", "Chrome");
+        ltOptions.put("project", "Belal");
+        ltOptions.put("w3c", true); // Ensure W3C compliance
+        ltOptions.put("debug", true); // Enable debugging if needed
+        browserOptions.setCapability("LT:Options", ltOptions);
+        // Construct the URL
+        String remoteUrl = "https://" + username + ":" + authkey + "@" + hub;
+        // Initialize RemoteWebDriver with SafariOptions
+        driver = new RemoteWebDriver(new URL(remoteUrl), browserOptions);
+    }
+
+    @Test
+    public void testFormSubmission() throws InterruptedException {
+        // Open the website
+        driver.get("https://the-internet.herokuapp.com/geolocation?trk=public_post_comment-text");
+        String pageTitle = driver.getTitle();
+        System.out.println("Page Title: " + pageTitle);
+        Assert.assertTrue(pageTitle.contains("The Internet"));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement whereIam=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button")));
+        whereIam.click();
+        Thread.sleep(3000);
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit(); // Close the browser after tests
+        }
+    }
+}
